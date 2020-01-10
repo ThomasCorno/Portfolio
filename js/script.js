1a79c7a17e778fc5a90cb7cafa83a696e646735a
ThomasCorno <thomas.corno@gmail.com>
2020-01-10 16:01:47 +0100

    Portfolio.1.2

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//Affichage de l'easter egg
+  function showPicture() {
+    var sourceOfPicture = "images/Answer_to_Life.webp";
+    var img = document.getElementById('Answer_to_Life')
+    img.src = sourceOfPicture.replace();
+    img.style.display = "block";
+  }
